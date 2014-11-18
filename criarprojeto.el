16b64dfed98c1995b3e69f1fadb668f5cee3f88e
maurojh <maurojh@gmail.com>
2014-11-18 14:23:54 -0200

    Create criarprojeto.el

---
+++ criarprojeto.el
@@ -0,0 +1,66 @@
+;; Acesse:
+;; http://ergoemacs.org/emacs/elisp_menu.html
+;; Para entender o cÃ³digo aqui
+;; Creating a new menu pane in the menu bar to the right of "Tools" menu
+(defun shell-command-on-buffer ()
+  "Asks for a command and executes it in inferior shell with current buffer
+as input."
+  (interactive)
+  (shell-command-on-region
+   (point-min) (point-max)
+;   (print projeto)
+   (read-shell-command "Shell command on buffer: ")))
+
+(defun construir ()
+  (cd (format "C:/%s" projeto))
+  (interactive)
+  (shell-command-on-region (point-min) (point-max) "ant debug"))
+
+(defun criar (nome target)
+  "Asks for a command and executes it in inferior shell with current buffer
+as input."
+  (interactive "sNome do projeto: \nnTarget: ")
+  (shell-command-on-region
+   (point-min) (point-max)
+   (format "C:/android-sdk/tools/android create project --target %d --name %s --path C:/%s --activity Principal --package br.ifsp.%s" target nome nome nome)
+   (setq projeto nome));; variável global com o nome do projeto
+   (find-file-existing (format "C:/%s/src/br/ifsp/%s/Principal.java" nome nome))   (find-file-existing (format "C:/%s/res/values/strings.xml" nome))
+   (find-file-existing (format "C:/%s/res/layout/main.xml" nome)))
+
+(defun listar () 
+  (interactive) 
+  (shell-command-on-region 
+   (point-min) (point-max) 
+   "C:/android-sdk/tools/android list targets"))
+
+(define-key-after
+  global-map
+  [menu-bar pcp2] ;; nome do menu
+  (cons "PCP2" (make-sparse-keymap "hoot hoot"))
+  'help-menu ) ;; depois do Help
+;; Para encontrar o nome de um menu use o comando:
+;; describe-key
+;; e clique no menu, exemplo: File -> Open:
+;; <menu-bar> <file> <open-file>
+;; quer dizer que o nome do menu Ã© [menu-bar file]
+
+;; Creating a menu item, under the menu by the id "[menu-bar mymenu]"
+;; creating another menu item
+(define-key
+  global-map
+  [menu-bar pcp2 construir]
+  '("Construir" . shell-command-on-buffer))
+
+(define-key
+  global-map
+  [menu-bar pcp2 criar]
+  '("Criar Projeto" . criar))
+
+(define-key
+  global-map
+  [menu-bar pcp2 listar]
+  '("Listar" . listar))
+
+
+;; code to remove the whole menu panel
+;; (global-unset-key [menu-bar mymenu])
